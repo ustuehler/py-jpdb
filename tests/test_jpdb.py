0c80a0ca118db512bf1e707abadcd7823ad027f6
Uwe Stuehler <uwe@bsdx.de>
2022-09-24 22:02:20 +0900

    feat: support exporting the review history for all cards

--- tests/test_jpdb.py
+++ tests/test_jpdb.py
@@ -19,6 +19,14 @@ class TestJPDB(unittest.TestCase):
     def test_due_items(self):
         self.assertIsInstance(self.jpdb.due_items, int)
 
+    def test_reviews(self):
+        reviews = self.jpdb.reviews
+        self.assertIsInstance(reviews, dict)
+        self.assertIn('cards_vocabulary_jp_en', reviews)
+        self.assertIn('cards_vocabulary_en_jp', reviews)
+        self.assertIn('cards_kanji_keyword_char', reviews)
+        self.assertIn('cards_kanji_char_keyword', reviews)
+
 
 if __name__ == '__main__':
     unittest.main()
